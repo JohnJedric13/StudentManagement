0a0fca57bd45d201598336b262c2621f615f1a83
JohnJedric13 <johnjedricalipat13@gmail.com>
2025-07-15 15:28:00 +0800

    upload source code

---
+++ src/Core/Crud.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace  Alipat\StudentManagement\Core;
+
+interface Crud {
+    public function create();
+    public function read();
+    public function update();
+    public function delete();
+}
+
